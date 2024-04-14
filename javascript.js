325d7ad983bbd10819604caf8d6abd3dc4a90be6
David Cruz <dmd.cruz12@gmail.com>
2024-04-14 23:09:45 +0100

    add button to change the square numbers on the container

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector(".container");
+const squareNoBtn = document.querySelector(".pixelsNoBtn");
+let pixelQtd = 16;
 
-const pixelQtd = 16;
-
-
+//Function to create the grid
 function createGrid(quantity) {
     for (let i = 0; i < quantity ; i++) {
         const row = document.createElement("div");
@@ -12,8 +12,28 @@ function createGrid(quantity) {
             let pixel = document.createElement("div");
             pixel.setAttribute("class","pixels");
             row.appendChild(pixel);
+            pixel.addEventListener("mouseover", colorPixel(pixel))
         }
     };
 };
+
+//Color grid pixels by hovering the mouse
+function colorPixel(pixel) {
+    pixel.addEventListener('mouseover',() => {
+        pixel.style.background = "black";
+    });
+};
+
+//Prompt number of squares per side for the new grid
+function squaresNo() {
+    return pixelQtd = prompt("Grid Size: how many squares do you want per side on the grid?")
+}
+
+
+squareNoBtn.addEventListener("click", () =>{
+    canvas.innerHTML = "";
+    pixelQtd = squaresNo();
+    createGrid(pixelQtd);
+});
 
 createGrid(pixelQtd);
